aaf6deced9a3b9f397033e68a76cddd3a377d7dc
TzuTing112200 <51486061+TzuTing112200@users.noreply.github.com>
2022-03-24 22:36:08 +0800

    Add files via upload

---
+++ OpenMP/CMakeFiles/MedianFilterOMP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MedianFilterOMP.dir/MedianFilter_omp.cpp.o"
+  "MedianFilterOMP.pdb"
+  "MedianFilterOMP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MedianFilterOMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
